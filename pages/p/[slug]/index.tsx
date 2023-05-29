70497989a0c4e283487e4b77e18ae40269f93f1a
Ryan Adhitama <ryanadhitama2@gmail.com>
2023-05-29 17:53:58 +0800

    add iframe

--- pages/p/[slug]/index.tsx
+++ pages/p/[slug]/index.tsx
@@ -1,4 +1,4 @@
-import { Hero, Layout } from "@/components";
+import { Box, Hero, Layout } from "@/components";
 import fs from "fs";
 import matter from "gray-matter";
 import Head from "next/head";
@@ -16,6 +16,18 @@ function Place({ frontmatter, content }: any) {
         <title>{frontmatter.title}</title>
       </Head>
       <Hero title={frontmatter.title} description={frontmatter.location} />
+      <Box className="place">
+        <Box className="container">
+          <Box className="place__content">
+            <iframe
+              className="place__map"
+              src={frontmatter.map}
+              width="100%"
+              height="450"
+            ></iframe>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 }
