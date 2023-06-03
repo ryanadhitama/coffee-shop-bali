1fc05d3a69454d30addbcafed0138f9108ce10f7
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-03 20:10:11 +0800

    test dir json

--- pages/api/places.ts
+++ pages/api/places.ts
@@ -1,18 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   try {
-    const placeDir = fs.readdirSync("_next/data");
-    const placeFile = fs.readFileSync(`_next/data/${placeDir[0]}/index.json`, "utf-8");
-    let places = JSON.parse(placeFile);
-    places = places?.pageProps?.places;
+    const jsonDirectory = path.join(process.cwd(), 'data/places');
+    const placeFiles = fs.readdirSync(jsonDirectory);
+    // const placeDir = fs.readdirSync("_next/data");
+    // const placeFile = fs.readFileSync(`_next/data/${placeDir[0]}/index.json`, "utf-8");
+    // let places = JSON.parse(placeFile);
+    // places = places?.pageProps?.places;
 
-    res.status(200).json({ data: places });
+    res.status(200).json({ data: placeFiles });
   } catch (error: any) {
     res.status(400).json({ message: error.message})
   }
