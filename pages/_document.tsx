be637e5695313844161a817b5256138dccff9888
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-05 07:53:17 +0800

    add parent title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,29 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta name="description" content={`Listing coffee shop in Bali`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://coffee-shop-bali.vercel.app" />
+        <meta
+          property="og:description"
+          content={`Listing coffee shop in Bali`}
+        />
+        {/* <meta property="og:image" content={image} /> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://coffee-shop-bali.vercel.app/"
+        />
+        {/* <meta property="twitter:title" content={`${title} - Coffee Shop Bali`} /> */}
+        <meta
+          property="twitter:description"
+          content={`Listing coffee shop in Bali`}
+        />
+        {/* <meta property="twitter:image" content={image} /> */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </Head>
       <body>
         <Main />
